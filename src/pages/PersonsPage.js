ee4c68604d23dff4cf760721e0f045d91c61ceff
mstanka <mice78.mm@gmail.com>
2021-08-19 13:29:54 +0200

    add pagination on persons page

--- src/pages/PersonsPage.js
+++ src/pages/PersonsPage.js
@@ -3,15 +3,34 @@ import { useState, useEffect } from 'react';
 
 const PersonsPage = () => {
   const [persons, setPersons] = useState(null);
-  const [page, setPage] = useState(1);
+  const [pageCurrent, setPageCurrent] = useState(
+    `https://swapi.dev/api/people/?page=1`,
+  );
+  const [pagePrev, setPagePrev] = useState(null);
+  const [pageNext, setPageNext] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
-      .get(`https://swapi.dev/api/people/?page=${page}`)
-      .then((response) => setPersons(response.data.results));
-  }, [page]);
+      .get(pageCurrent)
+      .then((response) => {
+        setPagePrev(response.data.previous);
+        setPageNext(response.data.next);
+        setPersons(response.data.results);
+      })
+      .catch((error) => setError(error));
+  }, [pageCurrent]);
 
+  if (error) return `Error: ${error.message}`;
   if (!persons) return <div>Loading....</div>;
+
+  const handleNextPage = () => {
+    setPageCurrent(pageNext);
+  };
+
+  const handlePrevPage = () => {
+    setPageCurrent(pagePrev);
+  };
 
   return (
     <div className="bg-gray-800 w-full max-w-screen-xl mx-auto py-10 text-center text-gray-300">
@@ -24,6 +43,18 @@ const PersonsPage = () => {
             {person.name}
           </li>
         ))}
+      </div>
+      <div className="relative mt-8">
+        {pagePrev && (
+          <button onClick={handlePrevPage} className="absolute left-10">
+            &#60; Previous Page
+          </button>
+        )}{' '}
+        {pageNext && (
+          <button onClick={handleNextPage} className="absolute right-10">
+            Next Page &#62;
+          </button>
+        )}
       </div>
     </div>
   );
