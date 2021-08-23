b33c55b93c26100fde2faa7e250603a299a782f3
mstanka <mice78.mm@gmail.com>
2021-08-23 20:49:13 +0200

    filter vehicles

--- src/pages/VehiclesPage.js
+++ src/pages/VehiclesPage.js
@@ -6,6 +6,8 @@ import Spinner from '../components/Spinner';
 
 const VehiclesPage = () => {
   const [vehicles, setVehicles] = useState(null);
+  const [filteredVehicles, setFilteredVehicles] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
   const [pageCurrent, setPageCurrent] = useState(
     `https://swapi.dev/api/vehicles/?page=1`,
   );
@@ -14,18 +16,22 @@ const VehiclesPage = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(pageCurrent)
-      .then((response) => {
-        setPagePrev(response.data.previous);
-        setPageNext(response.data.next);
-        setVehicles(response.data.results);
-      })
-      .catch((error) => setError(error));
+    const fetchVehicles = () => {
+      axios
+        .get(pageCurrent)
+        .then((response) => {
+          setPagePrev(response.data.previous);
+          setPageNext(response.data.next);
+          setVehicles(response.data.results);
+          setFilteredVehicles(response.data.results);
+        })
+        .catch((error) => setError(error));
+    };
+    fetchVehicles();
   }, [pageCurrent]);
 
   if (error) return `Error: ${error.message}`;
-  if (!vehicles) return <Spinner />;
+  if (!vehicles || !filteredVehicles) return <Spinner />;
 
   const handleNextPage = () => {
     setPageCurrent(pageNext);
@@ -35,10 +41,49 @@ const VehiclesPage = () => {
     setPageCurrent(pagePrev);
   };
 
+  const handelSearchInput = (e) => {
+    handleChange(e.target.value);
+  };
+
+  const handleChange = (value) => {
+    setSearchTerm(value);
+    filterVehicles(value);
+  };
+
+  const filterVehicles = (value) => {
+    const lowercasedValue = value.toLowerCase().trim();
+
+    if (lowercasedValue === '') {
+      setFilteredVehicles(vehicles);
+    } else {
+      const filteredData = vehicles.filter((vehicle) => {
+        return Object.keys(vehicle).some((key) =>
+          vehicle[key].toString().toLowerCase().includes(lowercasedValue),
+        );
+      });
+      setFilteredVehicles(filteredData);
+    }
+  };
+
   return (
     <Container>
+      <div className="py-10">
+        <label htmlFor="search-form">
+          <input
+            type="text"
+            id="search-form"
+            value={searchTerm}
+            onChange={handelSearchInput}
+            placeholder="Type to search..."
+            className="text-gray-800 p-1 max-w-xs w-full rounded"
+          />
+        </label>
+      </div>
+      {filteredVehicles.length === 0 && (
+        <p className="text-center">There is nothing to display!</p>
+      )}
       <div className="grid gap-2 justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {vehicles.map((vehicle) => (
+        {filteredVehicles.map((vehicle) => (
           <Vehicle
             key={vehicle.url}
             url={vehicle.url}
